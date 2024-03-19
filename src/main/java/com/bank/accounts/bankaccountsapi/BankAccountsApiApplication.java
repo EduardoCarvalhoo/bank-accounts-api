9ec7976dde49638f1fc37f130e46c64eae7dd8b0
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2024-03-18 23:06:29 -0300

    first commit

---
+++ src/main/java/com/bank/accounts/bankaccountsapi/BankAccountsApiApplication.java
@@ -0,0 +1,13 @@
+package com.bank.accounts.bankaccountsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/accounts/bankaccountsapi/BankAccountsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.accounts.bankaccountsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
